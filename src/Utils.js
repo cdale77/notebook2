5c1c8aa1bcc9ea010f9ab84fce81d7ea85d13d17
Brad Johnson <cdale77@gmail.com>
2018-01-23 14:28:31 -0800

    adds Utils

---
+++ src/Utils.js
@@ -0,0 +1,22 @@
+import Store from "./Store";
+
+const Utils = {
+
+  debounce: (func, wait = 500, immediate) => {
+    let timeout;
+    return function() {
+      let context = this,
+        args = arguments;
+      let later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+};
+
+export default Utils;
