1a6030fc6064673890e2985813bb36e0252f4020
Brad Johnson <cdale77@gmail.com>
2018-02-21 16:40:59 -0800

    Adds Note actions

---
+++ src/actions/NoteActions.js
@@ -0,0 +1,47 @@
+import Constants from "../Constants";
+
+const NoteActions = {
+  createNoteSuccess: note => {
+    return {
+      type: Constants.ACTIONS.CREATE_NOTE_SUCCESS,
+      note: note
+    };
+  },
+  createNoteFailure: message => {
+    return {
+      type: Constants.ACTIONS.CREATE_NOTE_FAILURE,
+      message: message
+    };
+  },
+  getNotesSuccess: noteList => {
+    return {
+      type: Constants.ACTIONS.GET_NOTES_SUCCESS,
+      noteList: noteList
+    };
+  },
+  getNotesFailure: message => {
+    return {
+      type: Constants.ACTIONS.GET_NOTES_FAILURE,
+      message: message
+    };
+  },
+  setCurrentNote: note => {
+    return {
+      type: Constants.ACTIONS.SET_CURRENT_NOTE,
+      note: note
+    };
+  },
+  clearCurrentNote: () => {
+    return {
+      type: Constants.ACTIONS.CLEAR_CURRENT_NOTE
+    };
+  },
+  destroyNote: noteId => {
+    return {
+      type: Constants.ACTIONS.DESTROY_NOTE,
+      noteId: noteId
+    };
+  }
+};
+
+export default NoteActions;
---
+++ src/test/actions/NoteActions.test.js
@@ -0,0 +1,71 @@
+import NoteActions from "../../actions/NoteActions";
+const note1 = {
+  bookId: "book1",
+  noteId: "1324",
+  text: "Test"
+};
+
+describe("NoteActions", () => {
+  it("should return the proper CREATE_NOTE_SUCCESS action", () => {
+    const result = NoteActions.createNoteSuccess(note1);
+    expect(result).toEqual({
+      type: "CREATE_NOTE_SUCCESS",
+      note: note1
+    });
+  });
+
+  it("should return the proper CREATE_NOTE_FAILURE action", () => {
+    const result = NoteActions.createNoteFailure("something went wrong");
+    expect(result).toEqual({
+      type: "CREATE_NOTE_FAILURE",
+      message: "something went wrong"
+    });
+  });
+
+  it("should return the proper GET_NOTES_SUCCESS action", () => {
+    const note2 = {
+      bookId: "book1",
+      noteId: "abcd",
+      text: "Foo"
+    };
+
+    const noteList = [note1, note2];
+
+    const result = NoteActions.getNotesSuccess(noteList);
+    expect(result).toEqual({
+      type: "GET_NOTES_SUCCESS",
+      noteList: noteList
+    });
+  });
+
+  it("should return the proper GET_NOTES_FAILURE action", () => {
+    const result = NoteActions.getNotesFailure("something went wrong");
+    expect(result).toEqual({
+      type: "GET_NOTES_FAILURE",
+      message: "something went wrong"
+    });
+  });
+
+  it("should return the proper SET_CURRENT_NOTE action", () => {
+    const result = NoteActions.setCurrentNote(note1);
+    expect(result).toEqual({
+      type: "SET_CURRENT_NOTE",
+      note: note1
+    });
+  });
+
+  it("should return the proper CLEAR_CURRENT_NOTE action", () => {
+    const result = NoteActions.clearCurrentNote();
+    expect(result).toEqual({
+      type: "CLEAR_CURRENT_NOTE"
+    });
+  });
+
+  it("should return the proper DESTROY_NOTE action", () => {
+    const result = NoteActions.destroyNote("abcd");
+    expect(result).toEqual({
+      type: "DESTROY_NOTE",
+      noteId: "abcd"
+    });
+  });
+});
