921871af7a9e7ef1d8bade9883ee51ce14b5a285
Brad Johnson <cdale77@gmail.com>
2018-03-05 20:37:47 -0800

    updates deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 echo "Deploying . . ."
 npm run build
-aws s3 sync build/ s3://notebook-nwjzlm/ --acl public-read --delete
+aws s3 sync build/ s3://notebook-nwjzlm/ --acl public-read --delete --cache-control max-age=60
